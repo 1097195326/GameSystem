b98e02d8f709d92597d4ecbfcfa6becef66db3a9
1097195326 <1097195326@qq.com>
2018-06-29 15:51:41 +0800

    Initial commit

---
+++ Source/GameFrame/GameFrameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameFrameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEFRAME_API AGameFrameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
